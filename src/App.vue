bfa7576f8ae46690942a762d5f3f29bd4fce46d3
Islam <aissiislam97@gmail.com>
2022-02-27 23:10:45 +0100

    added the components to the app

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,32 @@
 <template>
   <div id="app">
-    <div>test</div>
+    <header />
+    <nav />
+    <about />
+    <experience />
+    <porfolio />
+    <contact />
+    <footer />
   </div>
 </template>
 
 <script>
+import About from "./components/about/About.vue";
+import Contact from "./components/contact/Contact.vue";
+import Experience from "./components/experience/Experience.vue";
+import Header from "./components/header/Header.vue";
+import Nav from "./components/nav/Nav.vue";
+import Porfolio from "./components/porfolio/Porfolio.vue";
 export default {
   name: "App",
-  components: {},
+  components: {
+    About,
+    Header,
+    Experience,
+    Porfolio,
+    Contact,
+  },
+  Nav,
 };
 </script>
 
