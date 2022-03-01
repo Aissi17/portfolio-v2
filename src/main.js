a755798833dc06d41ce91578ac25648fd3eb5d18
Islam <aissiislam97@gmail.com>
2022-03-01 22:06:20 +0100

    imported fonts awesome

--- src/main.js
+++ src/main.js
@@ -3,6 +3,28 @@ import App from './App.vue'
 
 import "@/assets/css/main.css";
 
+/* import the fontawesome core */
+import {
+  library
+} from '@fortawesome/fontawesome-svg-core'
+
+/* import specific icons */
+import {
+  faUserSecret
+} from '@fortawesome/free-solid-svg-icons'
+
+import brands from "@fortawesome/fontawesome-free-brands";
+/* import font awesome icon component */
+import {
+  FontAwesomeIcon
+} from '@fortawesome/vue-fontawesome'
+
+/* add icons to the library */
+library.add(faUserSecret, brands)
+
+/* add font awesome icon component */
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
 Vue.config.productionTip = false
 
 new Vue({
